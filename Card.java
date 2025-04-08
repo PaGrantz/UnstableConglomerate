a1901ec0f9038cc2aa57c998877fb0868b1bfc7c
PaGrantz <paige.grantz@gmail.com>
2025-04-08 10:36:38 -0500

    Card done?

--- Card.java
+++ Card.java
@@ -1,15 +1,15 @@
 class Card{
 	//Fields
-	String suit;
-	boolean faceCard;
-	String face;
-	int value;
-	int order;
+	private String suit;
+	private boolean faceCard;
+	private String face;
+	private int value;
+	private int order;
 	//Static fields
-	String HEART = heart;
-	String DIAMOND = diamond;
-	String CLUB = club;
-	String SPADE = spade;
+	public String HEART = heart;
+	public String DIAMOND = diamond;
+	public String CLUB = club;
+	public String SPADE = spade;
 	//-------------------
 	
 	//Constructors
@@ -34,13 +34,16 @@ class Card{
 		face = f;
 		switch (f){
 			case K: order = 13;
-						//Other Cases
+			case Q: order = 12;
+			case J: order = 11;
+			case A: order = 1;
 		}
 	}
 	//--------------------
 	
 	//Getters
 	public int getValue() { return value; }
+	public int getOrder() { return order; }
 	public String getSuit() { return suit; }
 	public boolean isFaceCard() { return faceCard; }
 	public String getFace() { return face; }
@@ -51,15 +54,17 @@ class Card{
 		return this.suit.equals(s);
 	}
 	public int compareValue(Card other){
-		if(this.value//FINISH COMPARE
+		return this.order-other.getOrder();
 	}
-
+	public boolean compareSuit(Card other){
+		return this.suit.equals(other.getSuit());
+	}
+	public String toString(){
+		if(faceCard || face.equals("A")){
+			return face + " of " + suit;
+		}
+		return "" + value + " of " +suit;
+	}
 	//-------------------
-	
-
-
-
-
-
 
 }
