263d5aa9765d01d4b14646c748a9c0f377b63739
PaGrantz <paige.grantz@gmail.com>
2025-04-09 10:12:26 -0500

    Made Deck class, basic constructor

---
+++ Deck.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Deck{
+	//fields
+	int numCards
+	ArrayList<Card> deck;
+	//---------
+	
+
+	public Deck(){
+		numCards = 0;
+		deck = new ArrayList<Card>();
+	}
+	public Deck(int n){
+		numCards = n;
+		deck = new ArrayList<Card>();
+		for(int i=0;i<n;i++){
+		//add all cards	
+		}
+	}
+}
